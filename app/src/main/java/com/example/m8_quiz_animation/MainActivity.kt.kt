a48049807b6cf753bf4499fed634a5ea5b1ef821
Galina Shamgunova <galinashamgunova@gmail.com>
2023-01-31 17:19:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/m8_quiz_animation/MainActivity.kt.kt
@@ -0,0 +1,19 @@
+package com.example.m8_quiz_animation
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skillbox_hw_quiz.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
