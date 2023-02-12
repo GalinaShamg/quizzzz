10e30142d8bfc1e23a182293ecf53823b3da3704
Galina Shamgunova <galinashamgunova@gmail.com>
2023-02-12 19:20:44 +0300

    dfdgf

--- app/src/main/java/com/example/m8_quiz_animation/navigation/FirstFragment.kt
+++ app/src/main/java/com/example/m8_quiz_animation/navigation/FirstFragment.kt
@@ -9,9 +9,14 @@ import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.m8_quiz_animation.R
 import com.example.m8_quiz_animation.databinding.FragmentFirstBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FirstFragment : Fragment() {
-
+    private val dateFormat = SimpleDateFormat("dd.MM.yyyy")
+    private val calendar = Calendar.getInstance()
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
@@ -35,10 +40,41 @@ class FirstFragment : Fragment() {
 
     private fun setListenersButtons() {
         binding.bReady.setOnClickListener {
-            binding.bToBegin.isVisible = !binding.bToBegin.isVisible
+            showDialog()
         }
         binding.bToBegin.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        binding.buttonDate.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dateDialog = MaterialDatePicker.Builder.datePicker()
+            .setTitleText(resources.getString(R.string.date))
+            .build()
+        dateDialog.addOnPositiveButtonClickListener { timeInMillis ->
+            calendar.timeInMillis = timeInMillis
+            binding.apply {
+                bReady.isEnabled = false
+                bToBegin.isEnabled = true
+            }
+            Snackbar.make(
+                binding.bReady,
+                dateFormat.format(calendar.time),
+                Snackbar.LENGTH_SHORT
+            )
+                .setBackgroundTint(resources.getColor(R.color.purple_500))
+                .setTextColor(resources.getColor(R.color.white))
+                .show()
+        }
+        dateDialog.addOnNegativeButtonClickListener {
+            binding.apply {
+                bReady.isEnabled = true
+                bToBegin.isEnabled = false
+            }
+        }
+        dateDialog.show(childFragmentManager, "DatePicker")
     }
 }
